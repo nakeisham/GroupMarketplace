f339ed1f317ff6b5dc44f482e678af618f4217c5
sabrinaayres <103266232+sabrinaayres@users.noreply.github.com>
2023-06-01 16:00:30 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -193,7 +193,20 @@
 								<!-- <td><span class="status completed">Completed</span></td> -->
 							</tr>
 						</tbody>
+					
 					</table>
+					
+					<div id="google_translate_element"></div>
+       <script type="text/javascript">
+       
+         function googleTranslateElementInit() {
+         new google.translate.TranslateElement({
+           pageLanguage: 'en', layout:
+           google.translate.TranslateElement.InlineLayout.HORIZONTAL, autoDisplay: false, includedLanguages: 'fr,de,es', gaTrack: true, gaId:'YOUR_API_KEY'}, google_translate_element');
+                                               }
+       }
+       </script>
+       <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 				</div>
                 
 			</div>
